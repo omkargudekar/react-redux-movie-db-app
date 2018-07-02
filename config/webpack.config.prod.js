a1afcd08368ee3bf99b1cb71c9ec4d9c7a657e7f
Omkar Gudekar <omkargudekar@gmail.com>
2018-07-02 18:49:12 +0530

    Changed webpack config to support CSS Modules

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -183,6 +183,8 @@ module.exports = {
                         importLoaders: 1,
                         minimize: true,
                         sourceMap: shouldUseSourceMap,
+                        modules: true,
+                        localIdentName: '[name]__[local]___[hash:base64:5]'
                       },
                     },
                     {
